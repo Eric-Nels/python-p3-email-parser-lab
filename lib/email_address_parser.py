eecaed6adb855e1efd1dbbbe5007909e8f2caa20
Eric Nelson <ernelson12o0@gmail.com>
2023-10-12 18:29:51 -0400

    Lab Complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_address):
+        self.email_address = email_address
+
+    def parse(self):
+
+        email_list = re.split(r'[,\s]+', self.email_address)
+
+        pattern = r'([A-z]+)(\.\w+)?@(\w+)\.com'
+
+        email_regex = re.compile(pattern)
+
+        final_list = [email for email in email_list if email_regex.match(email)]
+
+        return sorted(final_list)
+
+    
